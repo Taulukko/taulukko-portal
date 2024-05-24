822ed08673d0f22921d666087034e89623b585e0
Edson Carli <gandnegro@gmail.com>
2024-05-24 09:44:35 +0100

    Adjust the server to read static files

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-import express from 'express';
-const app = express();
+ 
 
+import express from 'express';
+import path from 'path'; 
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+
+const app = express();
+const port = 3000;
+
+// Obter __dirname em um módulo ES6
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+// Servir arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota padrão que carrega o 'index.html'
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const port = parseInt(process.env.PORT) || 8080;
+// Iniciar o servidor
 app.listen(port, () => {
-  console.log(`helloworld: listening on port ${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
