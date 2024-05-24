06d7a5066b2f9c70d721cdd8c51e185b5ecd9d88
Edson Carli <gandnegro@gmail.com>
2024-05-24 13:31:02 +0100

    Add redirect for 404 pages

--- index.js
+++ index.js
@@ -11,12 +11,17 @@ const port =  parseInt(process.env.PORT) || 8080;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+//midleware to redirect content to public
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
- 
+//midleware to redirect content unknown to public
+app.use((req, res, next) => {
+  res.redirect('/');
+});
 app.listen(port, () => {
   console.log(`Taulukko: listening on port ${port}`);
 });
+
